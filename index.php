ad4e6a9db50300b9e63c971bc771fba74c519270
omkar192003 <138887590+omkar192003@users.noreply.github.com>
2024-10-08 20:36:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -96,36 +96,6 @@ https://templatemo.com/tm-588-ebook-landing
         </nav>
 
 
-      <section class="book-section section-padding" id="section_2">
-            <div class="container">
-                <div class="row">
-
-                    <div class="col-lg-6 col-12">
-                        <img src="tablet-screen-contents.jpg" class="img-fluid" alt="">
-                    </div>
-
-                    <div class="col-lg-6 col-12">
-                        <div class="book-section-info">
-                            <h6>Modern &amp; Creative</h6>
-
-
-                            <h2 class="mb-4">About The Book</h2>
-
-                            <p>Credit goes to <a rel="nofollow" href="https://freepik.com" target="_blank">FreePik</a>
-                                for images used in this ebook landing page template. You may browse FreePik to download
-                                more free images for your website.</p>
-
-                            <p>TemplateMo is one of the best websites to download free CSS templates for any purpose.
-                                This is an ebook landing page template using Bootstrap 5 CSS layout.</p>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </section>
-
-
-
         <section class="featured-section">
             <div class="container">
                 <div class="row">
@@ -160,8 +130,6 @@ https://templatemo.com/tm-588-ebook-landing
                 </div>
             </div>
         </section>
-
-
         <section class="py-lg-5"></section>
 
 
